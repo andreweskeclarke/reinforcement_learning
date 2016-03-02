758c9faf0ca88648c0e7a4afc64572d19360469a
andreweskeclarke <andreweskeclarke@gmail.com>
2016-03-02 19:18:05 +0000

    More refactoring

--- src/tetris/tetris_agent.py
+++ src/tetris/tetris_agent.py
@@ -21,7 +21,7 @@ ACTION_INDEX = 1
 REWARD_INDEX = 2
 STATE1_INDEX = 3
 
-BUFFER_SIZE = 50000
+BUFFER_SIZE = 500000
 
 class Agent():
     def __init__(self):
@@ -37,6 +37,7 @@ class Agent():
         self.rewards = np.zeros([BUFFER_SIZE], dtype=np.int32)
         self.interesting_indexes = list()
         self.current_episode_length = 0
+        self.training_runs = 0
 
     def exploit(self):
         # Simple linear exploit, max at 90% exploitations
@@ -51,7 +52,7 @@ class Agent():
         if self.exploit():
             vals = self.model.predict(np.array(state, ndmin=4), verbose=0)
             if random.random() < 0.005:
-                print('Some predicted values for a board:')
+                print('Some predicted values for a board. Trained against {} examples so far.'.format(self.training_runs))
                 print(np.array(state, ndmin=4))
                 print('[ROTATE_LEFT, ROTATE_RIGHT, MOVE_RIGHT, MOVE_LEFT, MOVE_DOWN, DO_NOTHING]')
                 print(vals)
@@ -81,8 +82,7 @@ class Agent():
             else:
                 indexes = [x % BUFFER_SIZE for x in range(self.current_pos - 1, self.current_pos - 1 - self.current_episode_length, -1)]
             DISCOUNT = 0.8
-            if reward > 0:
-                self.interesting_indexes.append(indexes)
+            self.interesting_indexes.append(indexes)
             for i, index in enumerate(indexes):
                 self.rewards[index] += reward * (DISCOUNT ** i) # TODO: some rounding issues here...
             self.train_on_indexes(indexes)
@@ -107,35 +107,36 @@ class Agent():
             print('Saved: {} and {}'.format(model_file, weights_file))
 
     def experience_replay(self):
-        indexes = np.random.randint(0, self.max_pos, N_REPLAYS_PER_ROUND)
-        self.train_on_indexes(indexes)
         if len(self.interesting_indexes) > 0:
-            self.train_on_indexes(random.choice(self.interesting_indexes))
+            indexes = list()
+            for i in range(0, N_REPLAYS_PER_ROUND):
+                indexes += random.choice(self.interesting_indexes)
+            random.shuffle(indexes)
+            self.train_on_indexes(indexes)
 
     def train_on_indexes(self, indexes):
+        self.training_runs += len(indexes)
         y = self.model.predict(self.states_t0[indexes], verbose=0)
-        DISCOUNT = 0.7
+        DISCOUNT = 0.8
         future_rewards = DISCOUNT*(np.amax(self.model.predict(self.states_t1[indexes], verbose=0), axis=1))
-        # print('Trained:')
-        # print(y[0])
         for i, a in enumerate(self.actions[indexes]):
             y[i][a] = self.rewards[indexes][i] + future_rewards[i]
         self.model.train_on_batch(self.states_t0[indexes], y)
-        # print(np.mean(y - self.model.predict(self.states_t0[indexes], verbose=0)))
 
     def init_model(self):
     #    self.model = model_from_json(open(max(glob.iglob('output/model_*.json'), key=os.path.getctime)).read())
     #    self.model.load_weights(max(glob.iglob('output/weights_*.h5'), key=os.path.getctime))
          self.model = Sequential()
-         self.model.add(Convolution2D(32, 3, 3, 
+         self.model.add(Convolution2D(16, 3, 3, 
                              activation='tanh', 
-                             init='he_normal',
+                             init='he_uniform',
                              input_shape=(1,22,10)))
+         self.model.add(Convolution2D(32, 4, 4, 
+                             activation='tanh', 
+                             init='he_uniform'))
          self.model.add(Flatten())
-         # Dense hidden layer
-         self.model.add(Dense(16, activation='tanh', init='he_normal'))
-         self.model.add(Dense(16, activation='tanh', init='he_normal'))
-         self.model.add(Dense(len(POSSIBLE_MOVES), activation='linear', init='he_normal'))
+         self.model.add(Dense(256, activation='relu', init='he_uniform'))
+         self.model.add(Dense(len(POSSIBLE_MOVES), activation='linear', init='he_uniform'))
          optim = RMSprop(lr=0.001, rho=0.9, epsilon=1e-06)
          self.model.compile(loss='mse', optimizer=optim)
 
