b154a4811653143e91b80de074c81abf072ed28b
Andrew Clarke <andreweskeclarke@gmail.com>
2016-03-12 09:04:42 -0600

    Add visualization script

---
+++ visualization/visualize.py
@@ -0,0 +1,27 @@
+from keras.models import Sequential, model_from_json
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import *
+from keras.optimizers import SGD, RMSprop
+from collections import deque
+import os
+import glob
+import numpy as np
+import queue
+import threading
+import time
+import random
+import math
+import matplotlib.pyplot as plt
+
+model = model_from_json(open(max(glob.iglob('model_*.json'), key=os.path.getctime)).read())
+model.load_weights(max(glob.iglob('weights_*.h5'), key=os.path.getctime))
+minimum = min([np.amin(a) for a in model.get_weights()])
+
+plt.axis('off')
+for i in range(0,64):
+    frame = plt.subplot(8,8,i+1)
+    frame.axes.get_xaxis().set_visible(False)
+    frame.axes.get_yaxis().set_visible(False)
+    plt.pcolor((model.get_weights()[0][i][0] + minimum), cmap=plt.get_cmap('Greys'))
+
+plt.savefig('conv_weights.png')
