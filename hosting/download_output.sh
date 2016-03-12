ff2593c0c2dfe4ea3d6f415d77e88e0a7b4046de
Andrew Clarke <andreweskeclarke@gmail.com>
2016-03-12 21:57:26 +0000

    Update index

--- hosting/download_output.sh
+++ hosting/download_output.sh
@@ -1,1 +1,2 @@
 scp -i ~/deep_learning.pem ubuntu@54.153.113.5:/home/ubuntu/src/reinforcement_learning/output/output.csv /www/data/
+scp -i ~/deep_learning.pem ubuntu@54.153.113.5:/home/ubuntu/src/reinforcement_learning/output/conv* /www/data/
